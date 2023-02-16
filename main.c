9311202d01ed2e1c2930fc0ca3b857272ca7fe6d
gyxxc <74789973+gyxxc@users.noreply.github.com>
2023-02-16 16:02:46 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+static int compar(const void *e1, const void *e2){
+  return *(int*)e1 - *(int*)e2;
+}
 int main(void){
+  int num[]={1, 2, 3, 4, 5, 4, 3, 2, 1};
+  qsort(num, numsSize, sizeof(int), compar);
   return 0;
 }
